03bc9a6d092164b047474cb3c40c5fad7821008e
Karel Hala <khala@redhat.com>
2024-02-13 16:22:46 +0100

    fix(release): fix release by pushing linted file (#94)

--- packages/insights-common-typescript/src/utils/__tests__/AlertUtils.test.ts
+++ packages/insights-common-typescript/src/utils/__tests__/AlertUtils.test.ts
@@ -27,67 +27,67 @@ describe('src/utils/AlertUtils', () => {
     it('Add notifications calls the store with the notification', () => {
         addNotification(NotificationType.INFO, 'foo', 'bar');
         expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
-          payload: expect.objectContaining({
-            variant: 'info',
-            title: 'foo',
-            description: 'bar',
-          })
+            payload: expect.objectContaining({
+                variant: 'info',
+                title: 'foo',
+                description: 'bar'
+            })
         }));
     });
 
     it('Add notifications accepts dismissable', () => {
         addNotification(NotificationType.INFO, 'foo', 'bar', true);
         expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
-          payload: expect.objectContaining({
-            variant: 'info',
-            title: 'foo',
-            description: 'bar',
-            dismissable: true,
-          })
+            payload: expect.objectContaining({
+                variant: 'info',
+                title: 'foo',
+                description: 'bar',
+                dismissable: true
+            })
         }));
     });
 
     it('addSuccessNotification creates a notification action with "success"', () => {
         addSuccessNotification('foo', 'bar');
         expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
-          payload: expect.objectContaining({
-            variant: 'success',
-            title: 'foo',
-            description: 'bar',
-          })
+            payload: expect.objectContaining({
+                variant: 'success',
+                title: 'foo',
+                description: 'bar'
+            })
         }));
     });
 
     it('addSuccessNotification creates a notification action with "info"', () => {
         addInfoNotification('foo', 'bar');
         expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
-          payload: expect.objectContaining({
-            variant: 'info',
-            title: 'foo',
-            description: 'bar',
-          })
+            payload: expect.objectContaining({
+                variant: 'info',
+                title: 'foo',
+                description: 'bar'
+            })
         }));
     });
 
     it('addSuccessNotification creates a notification action with "warning"', () => {
         addWarningNotification('foo', 'bar');
         expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
-          payload: expect.objectContaining({
-            variant: 'warning',
-            title: 'foo',
-            description: 'bar',
-          })
+            payload: expect.objectContaining({
+                variant: 'warning',
+                title: 'foo',
+                description: 'bar'
+            })
         }));
     });
 
     it('addSuccessNotification creates a notification action with "danger"', () => {
         addDangerNotification('foo', 'bar');
         expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
-          payload: expect.objectContaining({
-            variant: 'danger',
-            title: 'foo',
-            description: 'bar',
-          })
+            payload: expect.objectContaining({
+                variant: 'danger',
+                title: 'foo',
+                description: 'bar'
+            })
         }));
     });
 
